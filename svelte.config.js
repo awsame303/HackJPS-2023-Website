451f43b6d66d5b81fbd14bdeed3af1e234f0ab75
pranavt <72770606+awsame303@users.noreply.github.com>
2025-04-27 16:15:48 -0400

    asdasd

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// Use a fallback for any routes that can't be prerendered
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/HackJPS-2023-Website' : ''
-		},
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// Ignore missing image files in src directory
-				if (path.startsWith('/src/lib/images/')) {
-					console.warn(`Ignoring ${path} as it's referenced from ${referrer}`);
-					return;
-				}
-				// Otherwise, fail the build
-				throw new Error(message);
-			}
-		}
+			// default: serverless Node.js;
+			// e.g. runtime: 'nodejs18.x' or edge: true
+		})
 	}
 };
 
